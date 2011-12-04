422038f14f59febf261f326a13e66f1a0231a839
usernme <mnairis@itcollege.ee>
2011-12-04 12:42:10 +0200

    Sammhaaval projekti requirementside poole. Nüüd peaks kõik vajalikud vaated algtasemel olemas olema. Miskipärast ei tunnista piiripunkts.nimetus olemasolu seepärast on VahtkonnaRedaktori select väljal praegu veel terve piiripunkt objekt.

---
+++ piir/src/main/java/ee/itcollege/i377/team28/web/YlemVKRedaktorController.java
@@ -0,0 +1,39 @@
+package ee.itcollege.i377.team28.web;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ee.itcollege.i377.team28.entities.PIIRIPUNKT;
+import ee.itcollege.i377.team28.entities.PIIRIVALVUR;
+
+@RequestMapping("/ylemvkredaktor/**")
+@Controller
+public class YlemVKRedaktorController {
+
+    @RequestMapping
+    public void get(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping
+    public String index(Model uiModel) {
+    	List<PIIRIPUNKT> pp = new ArrayList<PIIRIPUNKT>();
+    	pp = PIIRIPUNKT.findAllPIIRIPUNKTS();
+    	
+    	uiModel.addAttribute("piiripunkts",pp);
+    	
+        return "ylemvkredaktor/index";
+    }
+}
